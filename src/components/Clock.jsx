19fdef7ef8ae51872169d048a9831e2cb43deae5
gangsejun0854-gsj <gangsejun0854@gmail.com>
2026-09-08 11:53:36 +0900

    은행

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,11 +1,17 @@
 // 1. import 구문자리
 //2. function 같은거
 //3. 함수형 컴포넌트 
-
+import { useState, useEffect } from "react";
 
 function Clock() {
-  const now = new Date();
-  return <span>{now.toLocaleTimeString("ko-KR")}</span>;
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const id = setInterval(() => setNow(new Date()), 1000);
+    return () => clearInterval(id);   // 정리 함수
+  }, []);
+
+  return <span className="muted">{now.toLocaleTimeString("ko-KR")}</span>;
 }
 
 export default Clock
